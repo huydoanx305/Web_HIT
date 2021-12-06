efe17d2e31b67e65e3bed9ea7e97a5c64ebb779c
Huy Doanx <huycntt03@gmail.com>
2021-12-06 16:10:11 +0700

    BTVN

---
+++ JS_Buoi3/Bai1.js
@@ -0,0 +1,29 @@
+//case1
+const case1 = "tranduong";
+
+// case2
+const case2 = "hitclubhiuhiu";
+
+// case3
+const case3 = "aabb";
+
+const count = (case1) => {
+    let map = {};
+    for(const value of case1)
+        map[value] = map.hasOwnProperty(value) ? ++map[value] : 1;   
+
+    for(const index in Object.keys(map))
+        if(Object.values(map)[index] == 1)   
+            return index;
+    return -1;
+    // for(const index in case1)
+    //     if(map[case1[index]] == 1)
+    //         return index;
+    // return -1;
+};
+console.log("case1");
+console.log("output: " + count(case1));
+console.log("case2");
+console.log("output: " + count(case2));
+console.log("case3");
+console.log("output: " + count(case3));
